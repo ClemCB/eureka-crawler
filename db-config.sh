8c3212f4300d69d671527ec5f72ae7cb8db1cb73
Clem Capel-Bird <clemcapelbird@gmail.com>
2017-05-09 11:23:36 +0100

    Fixing database names in db config

--- db-config.sh
+++ db-config.sh
@@ -29,8 +29,8 @@ function create_tables_for_development() {
   echo "Creating tables for development database"
   dbname="beetle_crawler_development"
   psql $dbname <<SQL
-    CREATE TABLE weburlsandtitles (id serial PRIMARY KEY, weburl varchar(65535), title varchar(65535));
-    SELECT * FROM weburlsandtitles;
+    CREATE TABLE weburlsandcontent (id serial PRIMARY KEY, weburl varchar(65535), title varchar(65535));
+    SELECT * FROM weburlsandcontent;
     CREATE TABLE weburls (id serial PRIMARY KEY, weburl varchar(65535));
     SELECT * FROM weburls;
 SQL
